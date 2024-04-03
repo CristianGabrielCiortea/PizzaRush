91d7170c7fabcbe491630f56a3c04bd08f19a889
tiberiuioan35@gmail.com <tiberiuioan35@gmail.com>
2024-04-03 14:56:49 +0300

    Add UI elements

--- PizzaRush/Assets/Scripts/CollisionHandler.cs
+++ PizzaRush/Assets/Scripts/CollisionHandler.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class CollisionHandler : MonoBehaviour
 {
     private Animator _animator;
     [SerializeField]
     private ParticleSystem _particleSystem;
+    [SerializeField]
+    public Sprite[] _images;
+    [SerializeField]
+    public Image _healthComponent;
+    private int _health = 4;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _healthComponent.sprite = _images[_health - 1];
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        switch(collision.gameObject.tag)
+        switch (collision.gameObject.tag)
         {
             case "Obstacle":
+                _health--;
+                _healthComponent.DOFade(0, 0.25f).OnComplete(() => {
+                    _healthComponent.sprite = _images[_health - 1];
+                    _healthComponent.DOFade(1, 0.25f);
+                });
+
+                if (_health == 0)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                    break;
+                }
+
                 _animator.SetTrigger("TriggerCollision");
                 _particleSystem.Play();
                 StartCoroutine(SetTriggerAfterDelay("UntriggerCollision", 1f));
